9b432ecc8913c46dd1b5de257e707b8b4130ea59
RonaldoVindas <rony1211@gmail.com>
2021-01-26 20:28:02 -0600

    Experimento 1: Datos agregados.

--- algoritmo_genetico_test.py
+++ algoritmo_genetico_test.py
@@ -7,5 +7,5 @@ class PruebaAlgoritmoGenetico(unittest.TestCase):
 
     def test_optimizar(self):
         dominio = DominioAGTSP('datos/ciudades_cr_pruebas.csv', 'Alajuela')
-        sol = optimizar(dominio, 100, 0.1, 0.5, 1000)
+        sol = optimizar(dominio, 100, 0.1, 0.7, 1000)
         self.assertTrue(dominio.validar(sol))
