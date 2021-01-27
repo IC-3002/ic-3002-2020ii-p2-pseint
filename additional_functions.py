a2ad41bf5337bbcd594c90ab69b35f5c0292282f
Eric Alpizar <ealpizarp@pop-os.localdomain>
2021-01-26 18:21:17 -0600

    Added experiment execution

--- additional_functions.py
+++ additional_functions.py
@@ -11,18 +11,61 @@ def calcular_tiempos_sa(repeticiones, ciudad_inicio, temperatura, enfriamiento):
 
     dominio_sa = DominioTSP('datos/ciudades_cr.csv', ciudad_inicio)
     tiempos = []
+    soluciones = []
+    mejor_indice = 0
 
     for i in range(repeticiones):
         
         start = time.time()
         sol = optimizar_sa(dominio_sa, temperatura, enfriamiento)
+        soluciones.append(sol)
         end = time.time()
-        
-        tiempos.append(end - start)
+        duracion = end - start
+        tiempos.append(duracion)
+        mejor_tiempo = min(tiempos)
 
-    plt.bar(range(repeticiones), tiempos, color ='blue', width = 0.6)
+        if duracion < mejor_tiempo:
+            mejor_indice = i
 
-    plt.xlabel("Tiempo de ejecucion") 
-    plt.ylabel("Tiempo")
+    indices = [str(x) for x in range(1, repeticiones+ 1)]
 
-    return tiempos
+    plt.bar(indices, tiempos, color ='blue', width = 0.6)
+
+    plt.xlabel("Numero de pruebas") 
+    plt.ylabel("Tiempo de ejecucion")
+
+    mejor_tiempo = min(tiempos)
+
+    resultado = []
+
+    resultado.append(mejor_tiempo)
+    resultado.append(soluciones[mejor_indice])
+
+    return resultado
+
+
+def comparar_resultados_tiempo(resultados, temperatura):
+
+    indices = [str(x) for x in range(1, 5+ 1)]
+    colores = ['orange', 'red', 'blue', 'cyan', 'green']
+    plt.bar(['0.50', '0.60', '0.70', '0.80', '0.90'], resultados, color =colores, width = 0.5)
+    plt.xlabel("Resultados a temperatura " + temperatura) 
+    plt.ylabel("Tiempo de ejecucion")
+
+def comparar_resultados_costo(resultados):
+    costos = []
+    dominio_sa = DominioTSP('datos/ciudades_cr.csv', 'Alajuela')
+    for i in range(len(resultados)):
+        costos.append(dominio_sa.fcosto(resultados[i]))
+    indices = [str(x) for x in range(1, 5+ 1)]
+    colores = ['orange', 'red', 'blue', 'cyan', 'green']
+    plt.bar(['0.50', '0.60', '0.70', '0.80', '0.90'], costos, color =colores, width = 0.5)
+    plt.xlabel("Resultados a temperatura ") 
+    plt.ylabel("Costo")
+
+
+
+
+
+
+
